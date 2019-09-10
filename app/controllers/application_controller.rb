edec30a1b8b7971df290aecfb68f67f9c3fdf9a7
eric-klimowich <eklimowich@hotmail.com>
2019-09-10 17:27:17 -0400

    Created method in ApplicationController to decode token

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,8 @@ class ApplicationController < ActionController::API
   def encode_token(payload)
     JWT.encode(payload, 'my_s3cr3t_c0d3')
   end
+
+  def decode_token(token)
+    JWT.decode(token, 'my_s3cr3t_c0d3')[0]
+  end
 end
