a38702b2dd90366cd2dd4b07010a36095e8ccaf9
eric-klimowich <eklimowich@hotmail.com>
2019-09-10 17:33:40 -0400

    Added method to accept authorization header and method to provide the decoded token

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,18 @@ class ApplicationController < ActionController::API
     JWT.encode(payload, 'my_s3cr3t_c0d3')
   end
 
-  def decode_token(token)
-    JWT.decode(token, 'my_s3cr3t_c0d3')[0]
+  def auth_header
+    request.headers['Authorization']
+  end
+
+  def decoded_token
+    if auth_header
+      token = auth_header.split(' ')[1]
+      begin
+        JWT.decode(token, 'my_s3cr3t_c0d3', true, algorithm: 'HS256')
+      rescue JWT::DecodeError
+        nil
+      end
+    end
   end
 end
