c2ec736758680327c67e2e5aab9c63b448f9d3bb
eric-klimowich <eklimowich@hotmail.com>
2019-09-24 13:42:33 -0400

    Added index to users controller

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,8 +1,13 @@
 class Api::V1::UsersController < ApplicationController
   skip_before_action :authorized, only: [:create]
 
-  def profile
-    render json: { user: UserSerializer.new(current_user) }, status: :accepted
+  # def profile
+  #   render json: { user: UserSerializer.new(current_user) }, status: :accepted
+  # end
+
+  def index
+    @users = User.all
+    render json: @users
   end
 
   def create
