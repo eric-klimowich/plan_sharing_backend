711cf741aed6b462b33dbd2c456d952fc65f807d
eric-klimowich <eklimowich@hotmail.com>
2019-10-12 09:53:13 -0400

    Created routes for subjects, reorganized routes layout

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,11 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :users, only: [:index, :show, :create]
+      resources :subjects, only: [:create]
+      resources :lessons, only: [:index, :create]
       post '/login', to: 'auth#create'
       get '/profile', to: 'users#profile'
       get '/logged_in_user', to: 'users#logged_in_user'
-      resources :lessons, only: [:index, :create]
     end
   end
 end
