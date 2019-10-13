9a3b0db5ad3d866272111d6c33b62d23f6bc2d89
eric-klimowich <eklimowich@hotmail.com>
2019-10-13 09:50:25 -0400

    Created create method for subjects in controller, added authenticate

--- app/controllers/api/v1/subjects_controller.rb
+++ app/controllers/api/v1/subjects_controller.rb
@@ -1,2 +1,17 @@
 class Api::V1::SubjectsController < ApplicationController
+  before_action :authenticate, only: [:create]
+
+  def create
+    @subject = Lesson.create(subject_params)
+    if @subject.valid?
+      render json: @subject, status: :accepted
+    else
+      render json: { errors: @subject.errors.full_messages }, status: :unprocessible_entity
+    end
+  end
+
+  private
+  def user_params
+    params.require(:subject).permit(:name, :user_id)
+  end
 end
