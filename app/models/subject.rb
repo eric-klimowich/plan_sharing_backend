566fc0c00ae709dc60c64d613d0e2759619cfbe2
eric-klimowich <eklimowich@hotmail.com>
2019-10-13 09:41:39 -0400

    Updated relationships between users, subjects, and lessons

--- app/models/subject.rb
+++ app/models/subject.rb
@@ -1,2 +1,4 @@
 class Subject < ApplicationRecord
+  belongs_to :user
+  has_many :lessons
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
   has_secure_password
-  has_many :lessons
+  has_many :subjects
   validates :username, uniqueness: { case_sensative: false }
 end
