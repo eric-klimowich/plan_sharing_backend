4a75b7a9b68b60b6cbe31954a6b4f33a802e29ea
eric-klimowich <eklimowich@hotmail.com>
2019-10-14 10:13:47 -0400

    Removed subject name from lessons table as it is only needed as a parameter to find/create subject

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_14_110434) do
+ActiveRecord::Schema.define(version: 2019_10_14_141233) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,7 +21,6 @@ ActiveRecord::Schema.define(version: 2019_10_14_110434) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "subject_id"
-    t.string "subject_name"
   end
 
   create_table "subjects", force: :cascade do |t|
