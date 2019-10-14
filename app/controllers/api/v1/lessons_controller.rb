d312aaf765cab8120abac9e8644d288340d9ea82
eric-klimowich <eklimowich@hotmail.com>
2019-10-14 17:29:54 -0400

    Created new parameter for lesson for name of grade

--- app/controllers/api/v1/lessons_controller.rb
+++ app/controllers/api/v1/lessons_controller.rb
@@ -11,11 +11,13 @@ class Api::V1::LessonsController < ApplicationController
     current_user_id = logged_in_user_id()
     if subject
       new_lesson_params = lesson_params.merge({:subject_id => subject.id})
+      new_lesson_params.delete("grade_name")
       new_lesson_params.delete("subject_name")
     else
       Subject.create(name: lesson_params["subject_name"], user_id: current_user_id)
       subject = Subject.find_by(name: lesson_params["subject_name"])
       new_lesson_params = lesson_params.merge({:subject_id => subject.id})
+      new_lesson_params.delete("grade_name")
       new_lesson_params.delete("subject_name")
     end
     @lesson = Lesson.create(new_lesson_params)
@@ -29,6 +31,6 @@ class Api::V1::LessonsController < ApplicationController
   private
 
   def lesson_params
-    params.require(:lesson).permit(:title, :content, :subject_name)
+    params.require(:lesson).permit(:title, :content, :grade_name, :subject_name)
   end
 end
