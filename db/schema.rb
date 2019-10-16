747197d5615ad94405678ec64a795418d91d2ae3
eric-klimowich <eklimowich@hotmail.com>
2019-10-16 18:26:03 -0400

    Grades migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_16_205655) do
+ActiveRecord::Schema.define(version: 2019_10_16_222426) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "grades", force: :cascade do |t|
+    t.string "grade_name"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "lessons", force: :cascade do |t|
     t.string "title"
