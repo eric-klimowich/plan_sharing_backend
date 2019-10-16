75ce7042918eb0ee60f75dd64586fb12dfae6cf8
eric-klimowich <eklimowich@hotmail.com>
2019-10-16 16:39:02 -0400

    Created create method for grades, added authenticate to grades controller

--- app/controllers/api/v1/grades_controller.rb
+++ app/controllers/api/v1/grades_controller.rb
@@ -1,2 +1,17 @@
 class Api::V1::GradesController < ApplicationController
+  before_action :authenticate, only: [:create]
+
+  def create
+    @grade = Grade.create(grade_params)
+    if @grade.valid?
+      render json: @grade, status: :accepted
+    else
+      render json: { errors: @grade.errors.full_messages }, status: :unprocessible_entity
+    end
+  end
+
+  private
+  def grade_params
+    params.require(:grade).permit(:name, :user_id)
+  end
 end
