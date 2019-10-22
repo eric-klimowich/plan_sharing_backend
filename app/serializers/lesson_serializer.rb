f4ebf843cfae129157dd263239f959eb6c19d8b0
eric-klimowich <eklimowich@hotmail.com>
2019-10-22 16:20:14 -0400

    Customized lesson data to be sent to front end

--- app/serializers/lesson_serializer.rb
+++ app/serializers/lesson_serializer.rb
@@ -1,3 +1,16 @@
 class LessonSerializer < ActiveModel::Serializer
-  attributes :id
+  attributes :lesson_data
+
+  def lesson_data
+    lesson_data_hash = {}
+
+    lesson_data_hash["id"] = object.id
+    lesson_data_hash["title"] = object.title
+    lesson_data_hash["description"] = object.description
+    lesson_data_hash["subject"] = object.user_grade_subject.subject.subject_name
+    lesson_data_hash["grade"] = object.user_grade_subject.user_grade.grade.grade_name
+    lesson_data_hash["user"] = object.user_grade_subject.user_grade.user.username
+
+    lesson_data_hash
+  end
 end
