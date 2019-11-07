6e14ba55bdf5e98f2c08d8a1ae9f8962f5db03b4
eric-klimowich <eklimowich@hotmail.com>
2019-11-07 11:23:25 -0500

    Created params and create method for comments

--- app/controllers/api/v1/comments_controller.rb
+++ app/controllers/api/v1/comments_controller.rb
@@ -1,2 +1,17 @@
 class Api::V1::CommentsController < ApplicationController
+  before_action :authenticate, only: [:create]
+
+  def create
+    @comment = Comment.create(comment_params)
+    if @comment.valid?
+      render json: @comment, status: :accepted
+    else
+      render json: { errors: @comment.errors.full_messages }, status: :unprocessible_entity
+    end
+  end
+
+  private
+  def comment_params
+    params.require(:comment).permit(:content, :user_id, :lesson_id)
+  end
 end
