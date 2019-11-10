291907c3a4a7d1f8488ad6dab806ae9c1afdfb3c
eric-klimowich <eklimowich@hotmail.com>
2019-11-10 08:51:34 -0500

    Created relationships between users and requests

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,6 @@ class User < ApplicationRecord
   has_many :grades, through: :user_grades
   has_many :favorite_lessons
   has_many :comments
+  has_many :requests
   validates :username, uniqueness: { case_sensative: false }
 end
