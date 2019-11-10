86fc3194def8f9cbbdd33fe16b7fb3cc3a674247
eric-klimowich <eklimowich@hotmail.com>
2019-11-10 08:49:34 -0500

    Created params and create method for requests

--- app/controllers/api/v1/requests_controller.rb
+++ app/controllers/api/v1/requests_controller.rb
@@ -1,2 +1,17 @@
 class Api::V1::RequestsController < ApplicationController
+  before_action :authenticate, only: [:create]
+
+  def create
+    @request = Request.create(request_params)
+    if @request.valid?
+      render json: @request, status: :accepted
+    else
+      render json: { errors: @request.errors.full_messages }, status: :unprocessible_entity
+    end
+  end
+
+  private
+  def request_params
+    params.require(:request).permit(:title, :content, :user_id)
+  end
 end
