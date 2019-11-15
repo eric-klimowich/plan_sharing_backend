5e0f45d0a386bf63b3da5e5545d89f0bf20cb9e9
eric-klimowich <eklimowich@hotmail.com>
2019-11-15 12:07:52 -0500

    Updated schema of users table, adding columns

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_15_165548) do
+ActiveRecord::Schema.define(version: 2019_11_15_170229) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -80,6 +80,10 @@ ActiveRecord::Schema.define(version: 2019_11_15_165548) do
     t.string "bio"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "country"
+    t.string "state"
+    t.string "city"
+    t.string "school"
   end
 
 end
