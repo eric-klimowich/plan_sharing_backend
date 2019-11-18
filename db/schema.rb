645c9e6dbc172ce58d1b1ad2bb254ff3336e2a03
eric-klimowich <eklimowich@hotmail.com>
2019-11-18 18:15:02 -0500

    Migration of responses and creation of schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_15_170229) do
+ActiveRecord::Schema.define(version: 2019_11_18_231352) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -50,6 +50,14 @@ ActiveRecord::Schema.define(version: 2019_11_15_170229) do
     t.string "title"
     t.text "content"
     t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "responses", force: :cascade do |t|
+    t.string "content"
+    t.integer "user_id"
+    t.integer "request_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
