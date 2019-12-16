3b36758f030a11811123cc18594ec7856001ed2d
eric-klimowich <eklimowich@hotmail.com>
2019-12-16 17:13:27 -0500

    Added create route to users

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :api do
     namespace :v1 do
-      resources :users, only: [:index, :show, :create]
+      resources :users, only: [:index, :show, :create, :update]
       resources :grades, only: [:create]
       resources :user_grades, only: [:create]
       resources :subjects, only: [:create]
